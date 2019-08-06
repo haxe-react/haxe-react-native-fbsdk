6f59d3166bb83068290086e9c70579bec2ff6405
Thomas FETIVEAU <thomas.fetiveau.tech@gmail.com>
2019-08-06 16:43:00 +0200

    Add LoginLManager logOut method

--- src/react/native/fbsdk/LoginManager.hx
+++ src/react/native/fbsdk/LoginManager.hx
@@ -6,6 +6,7 @@ import js.Promise;
 extern class LoginManager {
 	// https://github.com/facebook/react-native-fbsdk/blob/master/js/FBLoginManager.js
 	static function logInWithReadPermissions(permissions:Array<String>):Promise<LoginResult>;
+	static function logOut():Void;
 }
 
 typedef LoginResult = {
